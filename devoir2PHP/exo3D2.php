b4962d41a2c0059e39b65a8ef0baeb16b19da8a6
noura99bentaher <62450003+noura99bentaher@users.noreply.github.com>
2020-04-04 11:15:20 +0100

    Update exo3D2.php

--- devoir2PHP/exo3D2.php
+++ devoir2PHP/exo3D2.php
@@ -1,14 +1,11 @@
-<?php include('exo3.php');?>
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <title>la date </title>
-<?php
 
 
-
-?>
 </head>
 <body>
 	<div>
@@ -26,10 +23,10 @@
     ?>
     </tr>
     
-<form action="<?php $_SERVER['PHP_SELF'] ?>" method="post" ?enctype="application/x-www-form-urlencoded">
+<form action='exo3.php' method="POST">
 <tr>
   <td>
-<select name="jour">
+<select type="date" name="jour" >
 <?php
 
  for($i=1;$i<32;$i++)
@@ -38,32 +35,25 @@ echo "<option value=".$i."> $i </option>";
 </select>
 </td>
 <td>
-<select name = "mois"> 
-<option value="1">1</option> 
-<option value="2">2</option> 
-<option value="3">3</option> 
-<option value="4">4</option> 
-<option value="5">5</option> 
-<option value="6">6</option> 
-<option value="7">7</option> 
-<option value="8">8</option> 
-<option value="9">9</option> 
-<option value="10">10</option> 
-<option value="11">11</option> 
-<option value="12">12</option> 
+<select type="date" name = "mois"> 
+<?php
+
+ for($i=1;$i<13;$i++)
+echo "<option value=".$i."> $i </option>";
+?>
 </select> 
 </td>
 <td>
-<select name = "annee">
+<select  type="date" name = "annee">
 <?php
-for($i=1980;$i<2020;$i++) 
+for($i=1990;$i<2025;$i++) 
 echo"<option value=".$i."> $i </option>"; 
 ?>
 </select>
 </td>
 </tr>
 <tr><td colspan="2">
-<br><input type="submit" value="Envoyer" name="ok" onclick="<?php val(); ?>">
+<br><input type="submit" value="Envoyer" name="OK">
 </td>
 </tr>
 </form>
@@ -74,4 +64,4 @@ echo"<option value=".$i."> $i </option>";
 
 
 </body>
-</html> 
+</html>
